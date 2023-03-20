b2443ed9171cdcd354805dfc998917df982268ff
Isabel <isabqm121@gmail.com>
2023-03-20 16:57:28 -0400

    secuencia FizzBuzz para 5 numeros

--- src/secuenciaFizzBuzz.test.js
+++ src/secuenciaFizzBuzz.test.js
@@ -6,4 +6,7 @@ describe("FizzBuzz", () => {
     it("genera secuencia de 3 numeros",()=>{
         expect(SecuenciaFizzBuzz(3)).toEqual(" 1 2 Fizz");
     });
+    it("genera secuencia de 5 numeros",()=>{
+        expect(SecuenciaFizzBuzz(5)).toEqual(" 1 2 Fizz 4 Buzz");
+    });
 });
