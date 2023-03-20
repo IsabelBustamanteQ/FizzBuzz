a0d4d30b3baafca1a8e3e657c420e58d7b6393eb
Isabel <isabqm121@gmail.com>
2023-03-20 16:51:06 -0400

    Secuencia FizzBuzz para 3 numeros

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -28,5 +28,8 @@ describe("FizzBuzz", () => {
     it("genera secuencia de 2 numeros",()=>{
         expect(SecuenciaFizzBuzz(2)).toEqual(" 1 2");
     });
+    it("genera secuencia de 3 numeros",()=>{
+        expect(SecuenciaFizzBuzz(3)).toEqual(" 1 2 Fizz");
+    });
 
 });
